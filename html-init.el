1e88506077ee37174670108b374990372abd3cf4
ryuever <liuyouchao111@gmail.com>
2013-07-29 12:11:43 +0900

    configure emacs for editing html files that contain js

--- html-init.el
+++ html-init.el
@@ -22,3 +22,12 @@ This works on the current region."
 ;;(load "~/.emacs.d/nxhtml-master/autostart.el")
 ;; disable background color
 ;;(setq mumamo-background-colors nil)
+
+
+(require 'multi-web-mode)
+(setq mweb-default-major-mode 'html-mode)
+(setq mweb-tags '((php-mode "<\\?php\\|<\\? \\|<\\?=" "\\?>")
+                  (js-mode "<script +\\(type=\"text/javascript\"\\|language=\"javascript\"\\)[^>]*>" "</script>")
+                  (css-mode "<style +type=\"text/css\"[^>]*>" "</style>")))
+(setq mweb-filename-extensions '("php" "htm" "html" "ctp" "phtml" "php4" "php5"))
+(multi-web-global-mode 1)
