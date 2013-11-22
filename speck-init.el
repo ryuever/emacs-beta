8b074b63e5230efdc7ca5b9173d80ba5581197cf
ryuever <liuyouchao111@gmail.com>
2013-11-22 17:01:04 +0900

    pack the isolate speck

--- speck-init.el
+++ speck-init.el
@@ -11,9 +11,16 @@
 ;; but also have some other related mode.. so what's the relate modes?????
 (add-hook 'text-mode-hook 'turn-spell-checking-on)
 
+
 ;;(setq ispell-personal-dictionary "~/Ubuntu One/aspell.en.pws")
 ;;(setq speck-aspell-home-dir "~/emacs/")
 ;;(setq speck-personal-dictionary-file "aspell.en.pws")
 ;;(setq speck-processs-dictionary-alist "~/Ubuntu One/")
 ; change Aspell process directory (aspell.en.prepl path)
 ;;(setq ispell-process-directory "~/Ubuntu One/")
+
+;;************************************************************************
+;; speck,  aspell and ispell is not a prerequisite
+;;************************************************************************
+(global-set-key (kbd "<f8>") 'speck-popup-menu-at-point)
+(global-set-key (kbd "C-S-+") 'speck-add-word)
