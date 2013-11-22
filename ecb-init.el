96f132be0d7c9da0a5e9f1b4bb0ea2cf88523e55
ryuever <liuyouchao111@gmail.com>
2013-11-22 16:53:44 +0900

    comment these statements to avoid error in Emacs 24.3.1

--- ecb-init.el
+++ ecb-init.el
@@ -1,15 +1,15 @@
 ;;************************************************************************
 ;; cedet  & ecb, cedet is shipped with emacs
 ;;************************************************************************
-(semantic-mode)
-(require 'semantic/analyze)
-(provide 'semantic-analyze)
-(provide 'semantic-ctxt)
-(provide 'semanticdb)
-(provide 'semanticdb-find)
-(provide 'semanticdb-mode)
-(provide 'semantic-load)
-(setq stack-trace-on-error t)
+;; (semantic-mode)
+;; (require 'semantic/analyze)
+;; (provide 'semantic-analyze)
+;; (provide 'semantic-ctxt)
+;; (provide 'semanticdb)
+;; (provide 'semanticdb-find)
+;; (provide 'semanticdb-mode)
+;; (provide 'semantic-load)
+;; (setq stack-trace-on-error t)
 ;;ecb
 (require 'ecb-autoloads)
 
